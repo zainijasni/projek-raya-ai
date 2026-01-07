72d8629e23b3ca7b503a5e2d3c33c4f311e47a5d
zainijasni <zaini.jasni@gmail.com>
2026-01-07 13:55:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ try:
     if "GOOGLE_API_KEY" in st.secrets:
         GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
         genai.configure(api_key=GOOGLE_API_KEY)
-        model = genai.GenerativeModel('gemini-1.5-flash-latest')
+        model = genai.GenerativeModel('gemini-1.5')
     else:
         st.error("Google API Key tiada dalam Secrets!")
 
@@ -97,3 +97,4 @@ with col2:
 
             except Exception as e:
                 st.error(f"Ralat Sistem: {e}")
+
